af9ac4c428eb28707f4c6fb48cb6d89a87dbea02
Scott Alter <salter10@msn.com>
2023-09-06 15:29:36 -0400

    latest code.

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ import traceback
 import requests
 import re 
 from flask import escape, make_response
+from concurrent.futures import ThreadPoolExecutor
+import googlemaps
 
 @functions_framework.http
 def hello_http(request):
@@ -46,11 +48,11 @@ def hello_http(request):
     
     #define functions for gsheets
 
-    #name is the unique identifier per page. name is the third entry. 
+    #name is the unique identifier per page. name is the fourth entry. 
     def getIndex(name, sheet):
         i=0
         for row in sheet:
-            if row[2] == name:
+            if row[3] == name:
                 return i
             i+=1
         return -1
@@ -60,39 +62,56 @@ def hello_http(request):
         if dType == "Industrial":
             index = getIndex(data["agent_name"],indSheet)
             if(index>0):
-                indSheet[index] = [data[ind] for ind in ["title","property_address","agent_name","agent_address","agent_phone","company","last_mailed","num_mailed","ready_to_mail","property_type","first_seen"]]
+                indSheet[index] = [data[ind] for ind in ["title","property_address","company","agent_name","agent_address","agent_phone","last_mailed","num_mailed","ready_to_mail","property_type","first_seen"]]
             else:
                 data["first_seen"] = datetime.now().strftime("%Y/%m/%d")
                 addRow(data, dType)
         else:
             index = getIndex(data["agent_name"],wetSheet)
             if(index>0):
-                wetSheet[index] = [data[ind] for ind in ["title","property_address","agent_name","agent_address","agent_phone","company","last_mailed","num_mailed","ready_to_mail","property_type","first_seen"]]
+                wetSheet[index] = [data[ind] for ind in ["title","property_address","company","agent_name","agent_address","agent_phone","last_mailed","num_mailed","ready_to_mail","property_type","first_seen"]]
             else:
                 data["first_seen"] = datetime.now().strftime("%Y/%m/%d")
                 addRow(data,dType)
 
     def addRow(data, dType):
         if dType == "Industrial":
-            indSheet.append([data[ind] for ind in ["title","property_address","agent_name","agent_address","agent_phone","company","last_mailed","num_mailed","ready_to_mail","property_type","first_seen"]])
+            indSheet.append([data[ind] for ind in ["title","property_address","company","agent_name","agent_address","agent_phone","last_mailed","num_mailed","ready_to_mail","property_type","first_seen"]])
         else:
-            wetSheet.append([data[ind] for ind in ["title","property_address","agent_name","agent_address","agent_phone","company","last_mailed","num_mailed","ready_to_mail","property_type","first_seen"]])
+            wetSheet.append([data[ind] for ind in ["title","property_address","company","agent_name","agent_address","agent_phone","last_mailed","num_mailed","ready_to_mail","property_type","first_seen"]])
     
     def getLastMailedDTO(name, sheet):
         index = getIndex(name, sheet)
         out = datetime(2023,1,1)
-        if(index>0):
-            dstr = str(sheet[index][7])
+        if(index>=0):
+            dstr = str(sheet[index][6])
+            print(f"dstr: {dstr}")
             try:
-                out = datetime(int(dstr[0:4]), int(dstr[5:7]), int(dstr[8:10]))
+                if dstr[0] == "'":
+                    dstr = dstr[1:]
             except:
+                pass
+            try:
+                dstr = dstr.split("/")
+                out = datetime(int(dstr[2]), int(dstr[0]), int(dstr[1]))
+            except:
+                try:
+                    print(f"failed to make datetime object. inputs were: {dstr[2]}, {dstr[0]}, {dstr[1]}")
+                except Exception as e: 
+                    print(f"no dstr or {e}")
                 pass
         return out
 
     def getLastMailed(name, sheet):
         index = getIndex(name, sheet)
         if(index>0):
-            return sheet[index][7]
+            dstr = sheet[index][6]
+            try:
+                if dstr[0] == "'":
+                    dstr = dstr[1:]
+            except:
+                pass
+            return dstr
         else:
             return ""
     
@@ -127,7 +146,7 @@ def hello_http(request):
 
         #load indJson
         try:
-            indJson = ajson["Industrial"] #this may need json.loads again.
+            indJson = ajson["Industrial"] 
         except Exception as e:
             print("unable to load json: " + str(e))
 
@@ -143,17 +162,33 @@ def hello_http(request):
 
 
    
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        futures = [executor.submit(getData,url) for url in [obj['url'] for obj in industrials]]
+
+    executor.shutdown(wait=True)
+
+    indData = [future.result().copy() for future in futures]
+
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        futures = [executor.submit(getData,url) for url in [obj['url'] for obj in wetlands]]
+
+    executor.shutdown(wait=True)
+
+    wetData = [future.result().copy() for future in futures]
+
     #industrials
     dataType = "Industrial"
-    for obj in industrials:
+    for data in indData:
         #[{title, property_address, agent_name, agent_address, agent_phone, company, last_mailed, num_mailed, ready_to_mail, property_type},$agent2 if 2 agents$]
-        data = getData(obj["url"]) 
         for entry in data:
             if len(indJson) > 0:
                 if entry["agent_name"] in [seen["agent_name"] for seen in indJson]:
                     entry["property_type"] = "industrial"
                     agent_index = getIndexByKey(indJson, "agent_name", entry["agent_name"])
                     lastMailedSheet = getLastMailedDTO(entry["agent_name"],indSheet)
+                    print(lastMailedSheet)
+                    if lastMailedSheet == "":
+                        lastMailedSheet = datetime(2023,1,1)
                     if (lastMailedSheet- datetime(2023,1,1)).total_seconds() > indJson[agent_index]["last_mailed"]:
                         indJson[agent_index]["last_mailed"] = (lastMailedSheet- datetime(2023,1,1)).total_seconds()
                         indJson[agent_index]["num_mailed"] += 1
@@ -186,21 +221,23 @@ def hello_http(request):
     
     #wetlands
     dataType = "Wetlands"
-    for obj in wetlands:
+    for data in wetData:
         #[{title, property_address, agent_name, agent_address, agent_phone, company, last_mailed, num_mailed, ready_to_mail, property_type},$agent2 if 2 agents$]
-        data = getData(obj["url"]) 
         for entry in data:
             if len(wetJson) > 0:
                 if entry["agent_name"] in [seen["agent_name"] for seen in wetJson]:
-                    entry["property_type"] = "industrial"
+                    entry["property_type"] = "vacant"
                     agent_index = getIndexByKey(wetJson, "agent_name", entry["agent_name"])
                     lastMailedSheet = getLastMailedDTO(entry["agent_name"],wetSheet)
+                    print(lastMailedSheet)
+                    if lastMailedSheet == "":
+                        lastMailedSheet = datetime(2023,1,1)
                     if (lastMailedSheet- datetime(2023,1,1)).total_seconds() > wetJson[agent_index]["last_mailed"]:
                         wetJson[agent_index]["last_mailed"] = (lastMailedSheet- datetime(2023,1,1)).total_seconds()
                         wetJson[agent_index]["num_mailed"] += 1
-                    entry["last_mailed"] = getLastMailed(entry["agent_name"],wetSheet)
                     entry["num_mailed"] = wetJson[agent_index]["num_mailed"]
                     entry["first_seen"] = (timedelta(seconds=wetJson[agent_index]["first_seen"])+datetime(2023,1,1)).strftime("%Y/%m/%d")
+                    entry["last_mailed"] = getLastMailed(entry["agent_name"],wetSheet)
                     if(now() - wetJson[agent_index]["last_mailed"]>90*24*60*60):
                         entry["ready_to_mail"] = "yes"
                     else:
@@ -209,7 +246,7 @@ def hello_http(request):
                 else:
                     wetJson.append({"agent_name":entry["agent_name"],"first_seen": now(),"last_mailed": 0, "num_mailed": 0})
                     entry["first_seen"] = datetime.now().strftime("%Y/%m/%d")
-                    entry["property_type"] = "industrial"
+                    entry["property_type"] = "vacant"
                     entry["last_mailed"] = ""
                     entry["num_mailed"] = 0
                     entry["ready_to_mail"] = "yes"
@@ -217,7 +254,7 @@ def hello_http(request):
             else:
                     wetJson.append({"agent_name":entry["agent_name"],"first_seen": now(),"last_mailed": 0, "num_mailed": 0})
                     entry["first_seen"] = datetime.now().strftime("%Y/%m/%d")
-                    entry["property_type"] = "industrial"
+                    entry["property_type"] = "vacant"
                     entry["last_mailed"] = ""
                     entry["num_mailed"] = 0
                     entry["ready_to_mail"] = "yes"
@@ -279,13 +316,13 @@ def getData(url):
     # Extract property address
     address_element = soup.find(['h4','span'], string = "Address: ")
     if address_element:
-        data_out[0]["property_address"] = address_element.next_sibling.get_text(strip=True)
+        data_out[0]["property_address"] = get_best_address(address_element.next_sibling.get_text(strip=True))
     
     # Extract the agent address
     agent_address_element = soup.find('div', class_='cta-address')
     if agent_address_element:
         agent_address_text = agent_address_element.get_text(strip=True)
-        data_out[0]["agent_address"] = agent_address_text
+        data_out[0]["agent_address"] = get_best_address(agent_address_text)
 
     # Extract the agent phone
     agent_phone_element = soup.find('span', class_='phone-number')
@@ -302,11 +339,12 @@ def getData(url):
     # Extract the agent names
     try:
         agent_names_elements = soup.find_all('span', class_='contact-name')
-        agent_names_list = [name.get_text(strip=True).replace("Power Broker","") for name in agent_names_elements]
+        agent_names_list = [f"{name.find('span','first-name').get_text(strip=True)} {name.find('span','last-name').get_text(strip=True)}" for name in agent_names_elements]
         if len(agent_names_list)>1:
             data_out.append(data_out[0].copy())
             data_out[1]["agent_name"] = agent_names_list[1]
         data_out[0]["agent_name"] = agent_names_list[0]
+        
     except Exception as e:
         print(f"unable to get agent names due to {e}. agent_names_list = {agent_names_list}")
     
@@ -334,3 +372,17 @@ def getIndexByKey(listIn, key, value):
 
 def now():
     return (datetime.now()-datetime(2023,1,1)).total_seconds()
+
+def get_best_address(input_string):
+    # Initialize the Google Maps client
+    client = googlemaps.Client(key="AIzaSyB5_K_2QmV_9MvzpwArNJD3VqcqE9o8WMw")
+
+    # Geocode the input string
+    geocode_result = client.geocode(input_string)
+    # Check if there are results
+    if geocode_result:
+        # Get the formatted address of the first result (best match)
+        best_address = geocode_result[0]['formatted_address']
+        return best_address
+    else:
+        return input_string
